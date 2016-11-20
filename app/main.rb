501f0372a4901a82c296d01469a41dc972c5c496
kurage0819 <whileinclude1@yahoo.co.jp>
2016-11-21 03:38:51 +0900

    test

--- app/main.rb
+++ app/main.rb
@@ -17,10 +17,10 @@ def main(argv)
 	leap = (stock*year.to_i)%daysInMonth
 	leapc = (stock*year.to_i)/daysInMonth
 
-	if day.to_i>daysInMonth || daysInWeek>daysInMonth
+	if day.to_i>daysInMonth || daysInWeek>daysInMonth　|| stock ==　0	
 		print("-1\n")
 
-	elsif leap != 0 && month.to_i > (daysInyear/daysInMonth)　|| stock ==　0	
+	elsif leap != 0 && month.to_i > (daysInyear/daysInMonth)
 		print("-1\n")
 	
 	else 
