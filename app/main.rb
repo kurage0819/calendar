32f4ec80ce6d758877d15997c7d2f87270fe45a7
kurage0819 <whileinclude1@yahoo.co.jp>
2016-11-21 01:44:20 +0900

    test

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,39 @@
 def main(argv)
-  argv.each do |v|
-    puts v
-  end
+
+	daysInyear=argv[0].to_i
+	daysInMonth=argv[1].to_i
+	daysInWeek=argv[2].to_i
+
+	year,month,day = argv[3].split("-")
+
+	stock = daysInyear%daysInMonth
+
+	leap = stock * year.to_i
+
+	leapct=0
+	loop do
+		if leap>daysInMonth
+			leapct+=1
+			leap-=daysInMonth
+		end
+		if leap<daysInMonth
+			break
+		end
+	end
+
+	if month.to_i>daysInMonth || daysInWeek>daysInMonth
+		print"-1\n"
+	else
+		week = ((daysInyear*year.to_i)+(daysInMonth*month.to_i)+(leapct*daysInMonth)+day.to_i)%daysInMonth
+
+		if week == 0
+			week+=daysInWeek
+		end
+
+
+		print (week+64).chr,"\n"
+
+	end
+
+
 end
