71f19b688d9521d83cdd31e7ac4c4be205890c79
kurage0819 <whileinclude1@yahoo.co.jp>
2016-11-29 23:03:03 +0900

    a

--- app/main.rb
+++ app/main.rb
@@ -9,13 +9,16 @@ def main(argv)
 	stock = daysInyear%daysInMonth	#余り日数
 	yearm = daysInyear/daysInMonth	#一年の月数
 
-	leap=stock*(year.to_i-1) 	#余り日数の合計
+	leap=stock*(year.to_i-1)	#余り日数の合計
 	leapday=leap/daysInMonth	#余り日数が何月分か
 
 	if day.to_i>daysInMonth || daysInWeek>daysInMonth
 		print("-1\n")
 
-	elsif leapday == 0 && month.to_i > yearm
+	#elsif leapday > 0 && (year.to_i % leapday != 0) && month.to_i > yearm 
+	elsif year.to_i % (daysInMonth/stock) != 0 && month.to_i > yearm 
+		print("-1\n")
+	elsif month.to_i > yearm 
 		print("-1\n")
 	
 	else 
